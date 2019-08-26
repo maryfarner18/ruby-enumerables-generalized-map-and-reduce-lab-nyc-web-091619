ca8f3f6d9a6f5d56dd31a985da8ef664059be51d
Mary Farner <maryfarner@gmail.com>
2019-08-26 01:43:13 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-# Your Code Here
+def map(arr)
+  i = 0 
+  result = []
+  while i < arr.length do
+    result[i] = yield(arr[i])
+    i += 1
+  end
+  result
+end
+
+def reduce(arr, start = 0)
+  result = start
+  i=0
+  while i < arr.length do
+    result = yield(arr[i], result)
+    i += 1
+  end
+  
+  ##############
+  # This if check may be "cheating" but I cannot figure out how to solve the issue otherwise. If I reverse result and arr[i] in line 15, then the falsey / nil test passes, but the truthy test doesnt because true && 0 = 0
+  if(result == nil)
+    return false
+  end
+  result
+end
