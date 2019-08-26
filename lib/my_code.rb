538d69bc43c9f887c06ab6abea404b43ed4c6cdd
Mary Farner <maryfarner@gmail.com>
2019-08-26 01:43:35 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,7 +17,7 @@ def reduce(arr, start = 0)
   end
   
   ##############
-  # This if check may be "cheating" but I cannot figure out how to solve the issue otherwise. If I reverse result and arr[i] in line 15, then the falsey / nil test passes, but the truthy test doesnt because true && 0 = 0
+  # This if check may be "cheating" but I cannot figure out how to solve the issue otherwise. If I reverse result and arr[i] in line 15, then the falsey / nil test passes, but the truthy test doesnt because 0 && true = 0
   if(result == nil)
     return false
   end
